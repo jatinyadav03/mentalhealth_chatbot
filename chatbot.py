1efb6066d1b4df330a0e4ebf040d66d8dedfa082
jatinyadav03 <jatinjyadav03@gmail.com>
2024-06-27 11:36:58 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,4 @@
+import streamlit as st
+import streamlit.components.v1 as components
+
+st.components.v1.iframe(src="https://console.dialogflow.com/api-client/demo/embedded/1ef1d10d-6a99-447f-83c5-a4606e1036fe", width=450, height=500, scrolling=False)
